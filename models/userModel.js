febadf5a2967e457a085bf0f743323ce41462c70
livieez <131332606+livieez@users.noreply.github.com>
2026-04-01 09:19:25 +0800

    updated user account fields

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
     role: { type: String, enum: ['customer', 'vendor'], default: 'customer' },
+    shippingAddress: {type: String,default: ''},
+    region: {type: String,default: ''},
     createdAt: { type: Date, default: Date.now }
 });
 
