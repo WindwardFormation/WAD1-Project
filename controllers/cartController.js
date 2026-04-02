88449629dfce90faa743a76b9a4b255635e56beb
ChaiWen4445 <chaiwen.teh.2025@computing.smu.edu.sg>
2026-04-02 13:06:49 +0800

    redirects user back to the products page when stock left is 0

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,4 +1,5 @@
 const Cart = require('../models/cartModel');
+const Product = require('../models/productModel');
 
 exports.getCartPage = async (req, res) => {
     const cartItems = await Cart.getCartItemsByUserId(req.session.userId);
@@ -7,7 +8,10 @@ exports.getCartPage = async (req, res) => {
 
 exports.addToCart = async (req, res) => {
     const { productId, quantity } = req.body;
-
+    const stockAmount = await Product.findStocks(productId);
+    if (stockAmount <= 0) {
+        return res.redirect("/products");
+    };
     await Cart.addOrIncrementItem(req.session.userId, productId, parseInt(quantity));
     res.redirect('/cart');
 };
